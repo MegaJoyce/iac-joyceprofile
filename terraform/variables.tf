45e302d73368fa9ffaea7f2d3dfd514a90a42001
MegaJoyce <joyceheyue@live.com>
2024-09-02 11:49:30 -0700

    fix the name of the cluster

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "k8s.joyceheyue.fun"
+  default     = "joyceprofile-eks-cluster"
 }
 
 ##
